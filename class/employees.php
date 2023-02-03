fccca1300a73222389cd6b38362bbcfeb626d84e
Ayrton Rodrigues <ayrtonrodrigues45@gmail.com>
2023-02-02 22:43:08 -0300

    subindo projeto

---
+++ class/employees.php
@@ -0,0 +1,128 @@
+<?php
+    class Employee{
+
+        // Connection
+        private $conn;
+
+        // Table
+        private $db_table = "users";
+
+        // Columns
+        public $id;
+        public $name;
+        public $email;
+        public $age;
+
+        // Db connection
+        public function __construct($db){
+            $this->conn = $db;
+        }
+
+        // GET ALL
+        public function getEmployees(){
+            $sqlQuery = "SELECT id, name, email, age FROM " . $this->db_table . "";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        // CREATE
+        public function createEmployee(){
+            $sqlQuery = "INSERT INTO
+                        ". $this->db_table ."
+                    SET
+                        name = :name, 
+                        email = :email, 
+                        age = :age";
+        
+            $stmt = $this->conn->prepare($sqlQuery);
+        
+            // sanitize
+            $this->name=htmlspecialchars(strip_tags($this->name));
+            $this->email=htmlspecialchars(strip_tags($this->email));
+            $this->age=htmlspecialchars(strip_tags($this->age));
+        
+            // bind data
+            $stmt->bindParam(":name", $this->name);
+            $stmt->bindParam(":email", $this->email);
+            $stmt->bindParam(":age", $this->age);
+        
+            if($stmt->execute()){
+                return true;
+            }
+            return false;
+        }
+
+        // UPDATE
+        public function getSingleEmployee(){
+            $sqlQuery = "SELECT
+                        id, 
+                        name, 
+                        email, 
+                        age
+                      FROM
+                        ". $this->db_table ."
+                    WHERE 
+                       id = ?
+                    LIMIT 0,1";
+
+            $stmt = $this->conn->prepare($sqlQuery);
+
+            $stmt->bindParam(1, $this->id);
+
+            $stmt->execute();
+
+            $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $this->name = $dataRow['name'];
+            $this->email = $dataRow['email'];
+            $this->age = $dataRow['age'];
+        }        
+
+        // UPDATE
+        public function updateEmployee(){
+            $sqlQuery = "UPDATE
+                        ". $this->db_table ."
+                    SET
+                        name = :name, 
+                        email = :email, 
+                        age = :age
+                    WHERE 
+                        id = :id";
+        
+            $stmt = $this->conn->prepare($sqlQuery);
+        
+            $this->name=htmlspecialchars(strip_tags($this->name));
+            $this->email=htmlspecialchars(strip_tags($this->email));
+            $this->age=htmlspecialchars(strip_tags($this->age));
+            $this->id=htmlspecialchars(strip_tags($this->id));
+        
+            // bind data
+            $stmt->bindParam(":name", $this->name);
+            $stmt->bindParam(":email", $this->email);
+            $stmt->bindParam(":age", $this->age);
+            $stmt->bindParam(":id", $this->id);
+        
+            if($stmt->execute()){
+                return true;
+            }
+            return false;
+        }
+
+        // DELETE
+        function deleteEmployee(){
+            $sqlQuery = "DELETE FROM " . $this->db_table . " WHERE id = ?";
+            $stmt = $this->conn->prepare($sqlQuery);
+        
+            $this->id=htmlspecialchars(strip_tags($this->id));
+        
+            $stmt->bindParam(1, $this->id);
+        
+            if($stmt->execute()){
+                return true;
+            }
+            return false;
+        }
+
+    }
+?>
---
+++ config/dbconnect.php
@@ -0,0 +1,21 @@
+<?php 
+    class Database {
+        private $host = "localhost";
+        private $dbname = "moovery_teste";
+        private $username = "root";
+        private $password = "";
+
+        public $conn;
+
+        public function getConnection(){
+            $this->conn = null;
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->username, $this->password);
+                $this->conn->exec("set names utf8");
+            }catch(PDOException $exception){
+                echo "Database could not be connected: " . $exception->getMessage();
+            }
+            return $this->conn;
+        }
+    }  
+?>
